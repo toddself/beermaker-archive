651aa2537b08cf457f151012933382b38a158ca7
Todd Kennedy <todd.kennedy@gmail.com>
2010-01-31 14:17:39 -0500

    adding license info

--- data_import.py
+++ data_import.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
+
+# Copyright (C) 2010 Todd Kennedy <todd.kennedy@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 from xml.dom import minidom
 
 import db
 from models import Hop, Grain, Extract, HoppedExtract, Yeast, Measures, Fining, Mineral, Flavor, Spice, Herb, Misc
 from algorithms import sg_from_yield, c2f
-
-
     
 def process_bt_database():
     ds = db.DataStore()
