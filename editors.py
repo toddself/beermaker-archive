0f128ef91555c07967021b8a8ccaa3c000e0f59e
Todd Kennedy <todd.kennedy@gmail.com>
2010-02-08 11:29:29 -0500

    working with calendar popup for brewed on date

--- editors.py
+++ editors.py
@@ -17,12 +17,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import wx
+import wx.cal as cal
 
 import guid
 import iconsrc
 
 from db import DataStore
-from models import Recipe, Batch
+from models import Recipe, Batch, BJCPStyle, BJCPCategory
 
 from base import BaseWindow
 
@@ -35,56 +36,57 @@ class RecipeEditor(wx.Frame, BaseWindow):
         self.tools = self.buildToolbar()
         self.SetSize((1024,800))
         
-        self.panel = wx.Panel(self, -1)
+        self.main_panel = wx.Panel(self, -1)
         self.main_sizer = wx.BoxSizer(wx.VERTICAL)
         self._doLayout(self.main_sizer)
-        self.panel.SetSizer(self.main_sizer)
+        self.main_panel.SetSizer(self.main_sizer)
         
         # top row elements: name, style, brewed on, brewed by
+        self.name_txt = wx.StaticText(self.main_panel, -1, "Name:")
+        self.name_ctrl = wx.TextCtrl(self.main_panel, -1, "")
+        self.style_txt = wx.StaticText(self.main_panel, -1, "Name:")
+        self.style_ctrl = wx.Choice(self.main_panel, -1, choices=self._getStyleChoices())
+        self.brewed_on_txt = wx.StaticText(self.main_panel, -1, "Brewed On:")
+        self.brewed_on_ctrl = wx.TextCtrl(self.main_panel, -1, "")
+        
         top_row = wx.BoxSizer(wx.HORIZONTAL)
         
 
-
-    # def _setLayout(self):
-    #     """
-    #     _setLayout returns a tuple containing multiple dictionaries.  these dictionaries are then used
-    #     to generate the layout for the window
-    #     """
-    # 
-    #     window = ({
-    #     'element': 'sizer',
-    #     'format': wx.HORIZONTAL,
-    #     'type': 'box',
-    #     'elements': (
-    #         {'element': 'txt',
-    #         'type': 'static',
-    #         'value': 'Name:',
-    #         'greed': 0,
-    #         'padding': 3,
-    #         'style': wx.ALL|wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL|wx.ADJUST_MINSIZE},
-    #         {'element': 'txt',
-    #         'type': 'ctrl',
-    #         'greed': 1,
-    #         'padding': 3,
-    #         'style': wx.ALL|wx.EXPAND|wx.ADJUST_MINSIZE|wx.ALIGN_CENTER_VERTICAL
-    #         'bind': ((wx.EVT_TEXT, self.textChanged),)},
-    #         )
-    #     },)
-    # 
-    # def _doLayout(self, top_sizer):
-    #     for widget in self._setLayout():
-    #         top_sizer.Add(*self._createLayoutElement(widget))
-    # 
-    # def _createLayoutElement(self, widget_dict):
-    #     for widget in widget_dict:
-    #         if widget['element'] == 'sizer':
-    #             if widget['type'] == 'box':
-    #                 newSizer = wx.BoxSizer(widget['format'])
-    #                 newSizer.Add(*self._createLayoutElement(widget))
-    #         elif widget['element'] == 'txt':
-    #             if widget['type']
+    def _getStyleChoices(self):
+        styles = []
+        for style in list(BJCPStyle.select()):
+            styles.append("%s: %s" % style.get_combined_category_id, style.name)
+        
+        return styles
+        
+    def _showCalendar(self, event):
+        # do we have a date?
+        if self.brewed_on_ctrl.GetValue != "":
             
-
+        
+        # make a new panel for the calendar to live on and generate the calendar widget
+        self.calendar_panel = wx.Panel(self.main_panel)
+        calendar = cal.CalendarCtrl(calendar_panel, -1. wx.DateTime_Now(), style=CAL_SUNDAY_FIRST|CAL_SHOW_SURROUNDING_WEEKS|CAL_SEQUENTIAL_MONTH_SELECTION)
+        self.Bind(wx.EVT_CALENDAR, self._selectDate, id=calendar.GetId())
+        
+        # make the sizer, add the calendar and set the sizer to the panel
+        calendar_sizer = wx.BoxSizer(wx.VERTICAL)
+        calendar_sizer.Add(calendar, 0, wx.ALL, 3)
+        self.calendar_panel.SetSizer(calendar_panel)
+        
+        # position and show the calendar
+        self.calendar_panel.Raise()
+        self.calendar_panel.SetPosition((0,0))
+        self.calendar_panel.Show()
+    
+    def _selectDate(self, event):
+        # we've got the date selected, so we'll hide the calendar
+        self.calendar_panel.Hide()
+        date = event.GetDate()
+        dt = str(date).split(' ')
+        s = ' '.join(str(s) for s in dt)
+        self.brewed_on_ctrl.SetValue(s)
+  
     def newRecipe(self):
         pass
 
@@ -149,3 +151,43 @@ class RE(wx.App):
 if __name__ == "__main__":
     R = RE(0)
     R.MainLoop()        
+    
+    
+    # def _setLayout(self):
+    #     """
+    #     _setLayout returns a tuple containing multiple dictionaries.  these dictionaries are then used
+    #     to generate the layout for the window
+    #     """
+    # 
+    #     window = ({
+    #     'element': 'sizer',
+    #     'format': wx.HORIZONTAL,
+    #     'type': 'box',
+    #     'elements': (
+    #         {'element': 'txt',
+    #         'type': 'static',
+    #         'value': 'Name:',
+    #         'greed': 0,
+    #         'padding': 3,
+    #         'style': wx.ALL|wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL|wx.ADJUST_MINSIZE},
+    #         {'element': 'txt',
+    #         'type': 'ctrl',
+    #         'greed': 1,
+    #         'padding': 3,
+    #         'style': wx.ALL|wx.EXPAND|wx.ADJUST_MINSIZE|wx.ALIGN_CENTER_VERTICAL
+    #         'bind': ((wx.EVT_TEXT, self.textChanged),)},
+    #         )
+    #     },)
+    # 
+    # def _doLayout(self, top_sizer):
+    #     for widget in self._setLayout():
+    #         top_sizer.Add(*self._createLayoutElement(widget))
+    # 
+    # def _createLayoutElement(self, widget_dict):
+    #     for widget in widget_dict:
+    #         if widget['element'] == 'sizer':
+    #             if widget['type'] == 'box':
+    #                 newSizer = wx.BoxSizer(widget['format'])
+    #                 newSizer.Add(*self._createLayoutElement(widget))
+    #         elif widget['element'] == 'txt':
+    #             if widget['type']
